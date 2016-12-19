7982a076f68094fc502c9a1862455511a4fb975e
kubazerg <kubazerg@users.noreply.github.com>
2016-12-19 15:16:13 +0600

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,13 +1,13 @@
-age=int(input('Введите возраст:'))
-if age > 0 and age < 7:
+age = int(input('Введите возраст:'))
+if 0 <= age < 7:
     print('Вам в детский сад')
 elif 7 <= age < 18:
     print('Вам в школу')
-elif age >= 18 and age < 25:
+elif 18 <= age <= 25:
     print('Вам в профессиональное учебное заведение')
-elif age >= 25 and age < 60:
+elif 25 < age <= 60:
     print('Вам на работу')
-elif age >= 60 and age < 120:
+elif 60<age<=120:
     print('Вам предоставляется выбор')
-elif age < 0 or age > 120:
-    print('Ошибка! Это программа для людей!!! '*5)
+else:
+    print('Ошибка! Это программа для людей!!!\n' * 5)
